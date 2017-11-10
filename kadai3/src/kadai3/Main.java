b1b29b5c95fc8c5d0b23f1e90ce6c9068beed9db
tuyopon <s17t230@stu.kagawa-u.ac.jp>
2017-11-10 19:46:47 +0900

    kadai1-3

---
+++ kadai3/src/kadai3/Main.java
@@ -0,0 +1,17 @@
+package kadai3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 0;
+
+		while(a >= b) {
+			System.out.printf("%d ",b );
+			b++;
+		}
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
