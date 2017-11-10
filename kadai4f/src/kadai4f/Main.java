9d135d16d56287c764d1bbdc049eca1994dcb87b
tuyopon <s17t230@stu.kagawa-u.ac.jp>
2017-11-10 19:56:24 +0900

    kadai1-4

---
+++ kadai4f/src/kadai4f/Main.java
@@ -0,0 +1,15 @@
+package kadai4f;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a =3;
+		if(a % 2 == 0) {
+			System.out.printf("偶数");
+		}else {
+			System.out.printf("奇数");
+		}
+
+	}
+
+}
