84cc3759028722a1c02eb5891b61c0a49cbd367b
tuyopon <s17t230@stu.kagawa-u.ac.jp>
2017-11-10 19:41:30 +0900

    kadai1-2

---
+++ kadai2/src/kadai2/Main.java
@@ -0,0 +1,17 @@
+package kadai2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a,b;
+		a = 4;
+		b = 2;
+		System.out.printf("足し算：%d\n",a + b );
+		System.out.printf("引き算：%d\n",a - b );
+		System.out.printf("掛け算：%d\n",a * b );
+		System.out.printf("割り算：%d\n",a / b );
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
